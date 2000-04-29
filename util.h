8a4937badae8f417a27b1ab77db6273020f8dac7
steve <steve>
2000-04-29 04:53:44 +0000

    missing header file.

---
+++ util.h
@@ -0,0 +1,40 @@
+#ifndef __util_H
+#define __util_H
+/*
+ * Copyright (c) 2000 Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+#if !defined(WINNT) && !defined(macintosh)
+#ident "$Id: util.h,v 1.1 2000/04/29 04:53:44 steve Exp $"
+#endif
+
+# include  <string>
+
+/*
+ * This function returns the first name of a hierarchical path, and
+ * sets the parameter to what's left. If there is no path, then the
+ * parameter is set to "".
+ */
+extern string parse_first_name(string&path);
+
+/*
+ * $Log: util.h,v $
+ * Revision 1.1  2000/04/29 04:53:44  steve
+ *  missing header file.
+ *
+ */
+#endif
