1bae5677288fcf44320309f757ba99a043d99753
steve <steve>
2001-02-11 02:15:52 +0000

    Add the netmisc.cc source file.

---
+++ netmisc.cc
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2001 Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+#if !defined(WINNT) && !defined(macintosh)
+#ident "$Id: netmisc.cc,v 1.1 2001/02/11 02:15:52 steve Exp $"
+#endif
+
+# include  "netlist.h"
+# include  "netmisc.h"
+# include  "PExpr.h"
+
+
+NetExpr* elab_and_eval(Design*des, NetScope*scope, const PExpr*pe)
+{
+      NetExpr*tmp = pe->elaborate_expr(des, scope);
+      if (tmp == 0)
+	    return 0;
+
+      if (NetExpr*tmp2 = tmp->eval_tree()) {
+	    delete tmp;
+	    tmp = tmp2;
+      }
+
+      return tmp;
+}
+
+
+/*
+ * $Log: netmisc.cc,v $
+ * Revision 1.1  2001/02/11 02:15:52  steve
+ *  Add the netmisc.cc source file.
+ *
+ */
+
