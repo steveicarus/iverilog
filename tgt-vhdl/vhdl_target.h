9f035108e18204b6186b6f9e51ba2cc61e07a818
Nick Gasson <nick@nickg.me.uk>
2008-06-04 14:59:04 +0100

    Stub code for translating expressions

--- tgt-vhdl/vhdl_target.h
+++ tgt-vhdl/vhdl_target.h
@@ -14,6 +14,8 @@ int draw_scope(ivl_scope_t scope, void *_parent);
 int draw_process(ivl_process_t net, void *cd);
 int draw_stmt(vhdl_process *proc, ivl_statement_t stmt);
 
+vhdl_expr *translate_expr(ivl_expr_t e);
+
 void remember_entity(vhdl_entity *ent);
 vhdl_entity *find_entity(const std::string &tname);
 
