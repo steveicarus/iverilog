8b6b111541aa5859137eb87763727c48ce3dcbea
Nick Gasson <nick@nickg.me.uk>
2008-07-18 11:58:26 +0100

    Add IVL_LPM_CMP_EQ

--- tgt-vhdl/lpm.cc
+++ tgt-vhdl/lpm.cc
@@ -248,6 +248,7 @@ static vhdl_expr *lpm_to_expr(vhdl_scope *scope, ivl_lpm_t lpm)
       return concat_lpm_to_expr(scope, lpm);
    case IVL_LPM_CMP_GE:
       return rel_lpm_to_expr(scope, lpm, VHDL_BINOP_GEQ);
+   case IVL_LPM_CMP_EQ:
    case IVL_LPM_CMP_EEQ:
       return rel_lpm_to_expr(scope, lpm, VHDL_BINOP_EQ);
    case IVL_LPM_PART_VP:
