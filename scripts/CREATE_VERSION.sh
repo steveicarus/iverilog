2353f91693513e5e3f7e98cf04e6d8b7c64c2451
Cary R <cygcary@yahoo.com>
2009-01-06 18:38:29 -0800

    Make the CREATE_VERSION script create the correct file.
    The version tag is now stored in the version_tag.h file
    and is then copied along with the VERSION into the master
    version.h file by the Makefile.

--- scripts/CREATE_VERSION.sh
+++ scripts/CREATE_VERSION.sh
@@ -8,6 +8,6 @@
 #    sh scripts/CREATE_VERSION.sh
 #
 
-echo "Building version.h with git describe"
+echo "Building version_tag.h with git describe"
 tmp=`git describe | sed -e 's;\(.*\);#define VERSION_TAG "\1";'`
-echo "$tmp" > version.h
+echo "$tmp" > version_tag.h
