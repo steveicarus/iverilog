fb6e1c202aadb7051e811748b0a584e97455821d
Cary R <cygcary@yahoo.com>
2009-01-14 09:04:46 -0800

    Update some copyright dates.
    Update tgt-stub to have a 2009 copyright and a few individual files.

--- nodangle.cc
+++ nodangle.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999 Stephen Williams (steve@icarus.com)
+ * Copyright (c) 1999-2009 Stephen Williams (steve@icarus.com)
  *
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
@@ -220,4 +220,3 @@ void nodangle(Design*des)
       } while (fun.scontinue || fun.econtinue);
 
 }
-
