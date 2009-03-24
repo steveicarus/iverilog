809044cbf810db68f764194e99dbc00c6a72020c
Cary R <cygcary@yahoo.com>
2009-03-24 10:57:15 -0700

    Add guards in CREATE_VERSION script.
    This patch adds the same guards that the other two scripts
    create for the version_tag.h file.

--- scripts/CREATE_VERSION.sh
+++ scripts/CREATE_VERSION.sh
@@ -10,4 +10,6 @@
 
 echo "Building version_tag.h with git describe"
 tmp=`git describe | sed -e 's;\(.*\);#define VERSION_TAG "\1";'`
-echo "$tmp" > version_tag.h
+echo "#ifndef VERSION_TAG" > version_tag.h
+echo "$tmp" >> version_tag.h
+echo "#endif" >> version_tag.h
