ed7e9f8be56844e1f1294badef960d21eb5e01f7
Cary R <cygcary@yahoo.com>
2009-09-30 18:28:55 -0700

    Check if the compiler supports C99 and better check for some math functions.
    Add a configuration check that says we would like to use C99 if the
    compiler supports it. Also change the lround(), nan(), fmin() and
    fmax() checks to look in the default libraries as well as the math
    library.
    
    Update the iverilog-vpi sh script to correctly quote CC since it
    may now have two words (gcc -std=gnu99).
    
    Add a missing int for main in draw_tt.c.

--- vvp/draw_tt.c
+++ vvp/draw_tt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2008 Stephen Williams (steve@icarus.com)
+ * Copyright (c) 2001-2009 Stephen Williams (steve@icarus.com)
  *
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
@@ -117,7 +117,7 @@ static void draw_oct_table()
       printf("};\n");
 }
 
-main()
+int main()
 {
       draw_hex_table();
       draw_oct_table();
