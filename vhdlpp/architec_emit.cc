a5ca9ea8be6cb19cfea79e88f287f1f2fbe46d81
Pawel Szostek <pawel.szostek@gmail.com>
2011-07-20 16:34:36 +0200

    Use separate containers for current and previous scopes
    This patch introduces in ScopeBase separate containers
    for declarations coming from the current scope and from
    the previous scopes.
    Until now, in one scope, all objects were kept in an stl map.
    When a scope was created inside other scopes, a shallow
    copy of the map was made. This solution was nice for
    name shadowing (in new scopes, when a name was
    encountered, the old objects were overridden by a new
    one), but didn't allow for distinguishing where the objects
    were allocated. As a result, it is impossible to know who
    the owner is and who should delete them.
    
    In this commit ScopeBase gets two containers: for old
    and new objects. If a ScopeBase is made from another
    ScopeBase object, all objects from the copied object
    go to an old_XXX container, where XXX depends on the
    type of the copied objects. When a ScopeBase object
    is deleted, the objects from new_XXX are deleted and
    the ones from old_XXX are not touched.
    
    This patch adds some complexity to the internals
    of ScopeBase, but leaves its interface unchanged.

--- vhdlpp/architec_emit.cc
+++ vhdlpp/architec_emit.cc
@@ -30,12 +30,16 @@ int Scope::emit_signals(ostream&out, Entity*entity, Architecture*arc)
 {
       int errors = 0;
 
-      for (map<perm_string,Signal*>::iterator cur = signals_.begin()
-		 ; cur != signals_.end() ; ++cur) {
+      for (map<perm_string,Signal*>::iterator cur = old_signals_.begin()
+		 ; cur != old_signals_.end() ; ++cur) {
 
 	    errors += cur->second->emit(out, entity, arc);
       }
+      for (map<perm_string,Signal*>::iterator cur = new_signals_.begin()
+         ; cur != new_signals_.end() ; ++cur) {
 
+        errors += cur->second->emit(out, entity, arc);
+      }
       return errors;
 }
 
@@ -43,12 +47,19 @@ int Architecture::emit(ostream&out, Entity*entity)
 {
       int errors = 0;
 
-      for (map<perm_string,struct const_t>::iterator cur = constants_.begin()
-		 ; cur != constants_.end() ; ++cur) {
+      for (map<perm_string,struct const_t*>::iterator cur = old_constants_.begin()
+         ; cur != old_constants_.end() ; ++cur) {
 
-	    out << "localparam " << cur->first << " = ";
-	    errors += cur->second.val->emit(out, entity, this);
-	    out << ";" << endl;
+        out << "localparam " << cur->first << " = ";
+        errors += cur->second->val->emit(out, entity, this);
+        out << ";" << endl;
+      }
+      for (map<perm_string,struct const_t*>::iterator cur = new_constants_.begin()
+         ; cur != new_constants_.end() ; ++cur) {
+
+        out << "localparam " << cur->first << " = ";
+        errors += cur->second->val->emit(out, entity, this);
+        out << ";" << endl;
       }
 
       errors += emit_signals(out, entity, this);
