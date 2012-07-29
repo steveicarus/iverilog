082418d73b3473c0842f2664a69b4d731b5cf0e9
Stephen Williams <steve@icarus.com>
2012-07-29 16:25:26 -0700

    Add missing netdarray.h header file.

---
+++ netarray.h
@@ -0,0 +1,48 @@
+#ifndef __netarray_H
+#define __netarray_H
+/*
+ * Copyright (c) 2012 Stephen Williams (steve@icarus.com)
+ * Copyright CERN 2012 / Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+# include  "LineInfo.h"
+# include  <vector>
+
+class netarray_t : public LineInfo {
+
+    public:
+      explicit netarray_t(const std::list<netrange_t>&packed);
+      ~netarray_t();
+
+      unsigned packed_width() const;
+
+    private:
+      std::list<netrange_t> packed_dims_;
+
+};
+
+inline netarray_t::netarray_t(const std::list<netrange_t>&packed)
+: packed_dims_(packed)
+{
+}
+
+netarray_t::~netarray_t()
+{
+}
+
+#endif
