747e656a0e106cd0e5eb04648882bff8da04551a
Maciej Suminski <maciej.suminski@cern.ch>
2014-09-29 11:38:54 +0200

    vhdlpp: Added ScopeBase::transfer_from() method.

--- vhdlpp/scope.h
+++ vhdlpp/scope.h
@@ -57,6 +57,9 @@ class ScopeBase {
       Signal* find_signal(perm_string by_name) const;
       Variable* find_variable(perm_string by_name) const;
       Subprogram* find_subprogram(perm_string by_name) const;
+	// Moves all signals, variables and components from another scope to
+	// this one. After the transfer new_* maps are emptied in the another scope.
+      void transfer_from(ScopeBase&ref);
 
     protected:
       void cleanup();
@@ -202,7 +205,7 @@ class ActiveScope : public ScopeBase {
       { map<perm_string, Subprogram*>::iterator it;
         if((it = use_subprograms_.find(name)) != use_subprograms_.end() )
             use_subprograms_.erase(it);
-        cur_subprograms_[name] = obj;;
+        cur_subprograms_[name] = obj;
       }
 
       void bind(Entity*ent)
