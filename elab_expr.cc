102d2d534f982ba3af64ec0a1a40206fb54f5777
Cary R <cygcary@yahoo.com>
2015-03-09 14:34:29 -0700

    Remove extra warning code.

--- elab_expr.cc
+++ elab_expr.cc
@@ -667,14 +667,6 @@ NetExpr* PEBComp::elaborate_expr(Design*des, NetScope*scope,
 		  des->errors += 1;
 		  return 0;
 	    }
-	    if (lp->expr_type() == IVL_VT_STRING ||
-		rp->expr_type() == IVL_VT_STRING) {
-		  cerr << get_fileline() << ": error: "
-		       << human_readable_op(op_)
-		       << " operator may not have REAL operands." << endl;
-		  des->errors += 1;
-		  return 0;
-	    }
 	    break;
 	  default:
 	    break;
