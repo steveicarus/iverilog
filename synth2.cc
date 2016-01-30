086ce04cf5825f8866d82c5fd2c7571de606ec49
Martin Whitaker <icarus@martin-whitaker.me.uk>
2016-01-30 17:01:13 +0000

    Temporary fix for br995 - assignment to memory not supported in synthesis.
    (cherry-picked from master branch)
    
    For now, output a "sorry" message.

--- synth2.cc
+++ synth2.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2015 Stephen Williams (steve@icarus.com)
+ * Copyright (c) 2002-2016 Stephen Williams (steve@icarus.com)
  *
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
@@ -102,6 +102,14 @@ bool NetAssignBase::synth_async(Design*des, NetScope*scope,
 
       NetNet*rsig = rval_->synthesize(des, scope, rval_);
       assert(rsig);
+
+      if (lval_->word() && ! dynamic_cast<NetEConst*>(lval_->word())) {
+	    cerr << get_fileline() << ": sorry: assignment to variable "
+		    "location in memory is not currently supported in "
+		    "synthesis." << endl;
+	    des->errors += 1;
+	    return false;
+      }
 
       NetNet*lsig = lval_->sig();
       if (!lsig) {
