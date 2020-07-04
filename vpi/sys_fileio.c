fc19d29269c148cb6325e399acf9ad16cb20ef4f
Cary R <cygcary@yahoo.com>
2020-07-03 23:14:50 -0700

    Update copyright

--- vpi/sys_fileio.c
+++ vpi/sys_fileio.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2018 Stephen Williams (steve@icarus.com)
+ * Copyright (c) 2003-2020 Stephen Williams (steve@icarus.com)
  *
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
