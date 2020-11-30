f4aff860e735b389a3dbbdd24b3cdd50399df0b6
umarcor <unai.martinezcorral@ehu.eus>
2020-11-30 08:13:53 +0100

    ci/win: remove 'mingw64' from 'test.sh'

--- .github/test.sh
+++ .github/test.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env sh
+
 git clone https://github.com/steveicarus/ivtest.git
 cd ivtest
 
 if [ "$OS" = 'Windows_NT' ]; then
-  $mingw64 perl update_msys2_report.pl
-  $mingw64 perl vvp_reg.pl
+  perl update_msys2_report.pl
+  perl vvp_reg.pl
   diff --strip-trailing-cr regression_report-msys2.txt regression_report.txt
-  $mingw64 perl vpi_reg.pl
 else
   perl vvp_reg.pl
   diff regression_report-devel.txt regression_report.txt
-  perl vpi_reg.pl
 fi
+
+perl vpi_reg.pl
