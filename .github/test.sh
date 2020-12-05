1bb355a95e913f563501d01aec71b446bdefb811
Martin Whitaker <icarus@martin-whitaker.me.uk>
2020-12-05 22:38:30 +0000

    Update CI test script to easily change version of expected results.

--- .github/test.sh
+++ .github/test.sh
@@ -3,13 +3,15 @@
 git clone https://github.com/steveicarus/ivtest.git
 cd ivtest
 
+version=devel
+
 if [ "$OS" = 'Windows_NT' ]; then
-  perl update_msys2_report.pl
+  perl update_msys2_report.pl $version
   perl vvp_reg.pl
-  diff --strip-trailing-cr regression_report-msys2.txt regression_report.txt
+  diff --strip-trailing-cr regression_report-msys2-$version.txt regression_report.txt
 else
   perl vvp_reg.pl
-  diff regression_report-devel.txt regression_report.txt
+  diff regression_report-$version.txt regression_report.txt
 fi
 
 perl vpi_reg.pl
