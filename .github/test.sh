0b991f5a28a16bb1d5e62a66a16dcd9b53e881ca
Martin Whitaker <icarus@martin-whitaker.me.uk>
2020-12-05 22:01:38 +0000

    Fix CI test script.

--- .github/test.sh
+++ .github/test.sh
@@ -7,11 +7,11 @@ cd ivtest
 
 if [ "$OS" = 'Windows_NT' ]; then
   perl update_msys2_report.pl
-  perl vvp_reg.pl $suffix
+  perl vvp_reg.pl -suffix=$suffix
   diff --strip-trailing-cr regression_report-msys2.txt regression_report.txt
 else
-  perl vvp_reg.pl $suffix
+  perl vvp_reg.pl -suffix=$suffix
   diff regression_report-devel.txt regression_report.txt
 fi
 
-perl vpi_reg.pl $suffix
+perl vpi_reg.pl -suffix=$suffix
