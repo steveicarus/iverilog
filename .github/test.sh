27986477fb7b49b7ed3a6d3465835b40efc77159
Martin Whitaker <icarus@martin-whitaker.me.uk>
2020-12-05 22:55:23 +0000

    Further fix to CI test script.

--- .github/test.sh
+++ .github/test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-suffix=-11
-
 git clone https://github.com/steveicarus/ivtest.git
 cd ivtest
 
+version=v11
+
 if [ "$OS" = 'Windows_NT' ]; then
-  perl update_msys2_report.pl
-  perl vvp_reg.pl -suffix=$suffix
-  diff --strip-trailing-cr regression_report-msys2.txt regression_report.txt
+  perl update_msys2_report.pl $version
+  perl vvp_reg.pl
+  diff --strip-trailing-cr regression_report-msys2-$version.txt regression_report.txt
 else
-  perl vvp_reg.pl -suffix=$suffix
-  diff regression_report-devel.txt regression_report.txt
+  perl vvp_reg.pl
+  diff regression_report-$version.txt regression_report.txt
 fi
 
-perl vpi_reg.pl -suffix=$suffix
+perl vpi_reg.pl
