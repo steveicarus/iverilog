1858c6a313373f0d1629d11dd85793152273bf8d
Lars-Peter Clausen <lars@metafoo.de>
2022-01-16 10:52:46 +0100

    Set regression test for explicit enum cast to supported
    Update the enum cast test to reflect that enum casts are now supported.
    
    Also add a small check to verify that the right value got assigned to the
    enum after the cast.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- ivtest/ivltests/br_gh386d.v
+++ ivtest/ivltests/br_gh386d.v
@@ -1,4 +1,4 @@
-// This is currently unsupported, but is legal code.
+// Test that cast to enum works in continuous assignments
 module test();
 
 typedef enum { a, b, c } enum_type;
@@ -6,5 +6,13 @@ typedef enum { a, b, c } enum_type;
 enum_type enum_value;
 
 assign enum_value = enum_type'(1);
+
+initial begin
+  if (enum_value == b) begin
+    $display("PASSED");
+  end else begin
+    $display("FAILED");
+  end
+end
 
 endmodule
