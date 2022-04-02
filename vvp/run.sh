bef0e0c37d3916b3bf38cc9405572c0781cfecd2
Hugh Perkins <hughperkins@gmail.com>
2022-04-01 21:36:10 -0400

    remove superfluous files

--- vvp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-make
-iverilog -g2012 -ohello.vvp hello.sv
-g++ -I../install.d/include/iverilog -c test_client.cpp 
-g++ -o test_client test_client.o -L../vvp -lvvp
-./test_client
