dba6798d128c2c444c13abc313c3a367dbd63a11
Lars-Peter Clausen <lars@metafoo.de>
2022-06-12 18:08:32 +0200

    Remove unused `PExpr::is_the_same()`
    The last user of the `PExpr::is_the_same()` method was removed in commit
    37b60a4c52a4 ("Clean up interface of the PWire class").
    
    Remove the method.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- PExpr.h
+++ PExpr.h
@@ -180,11 +180,6 @@ class PExpr : public LineInfo {
       virtual bool is_collapsible_net(Design*des, NetScope*scope,
                                       NetNet::PortType port_type) const;
 
-	// This method returns true if that expression is the same as
-	// this expression. This method is used for comparing
-	// expressions that must be structurally "identical".
-      virtual bool is_the_same(const PExpr*that) const;
-
     protected:
       unsigned fix_width_(width_mode_t mode);
 
@@ -638,8 +633,6 @@ class PENumber : public PExpr {
 					 NetScope*scope,
 					 bool is_cassign,
 					 bool is_force) const;
-
-      virtual bool is_the_same(const PExpr*that) const;
 
     private:
       verinum*const value_;
