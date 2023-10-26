0b22810a257ed2b5c72a2f31ba440ff78311539b
Johan Euphrosine <proppy@google.com>
2023-10-26 11:30:56 +0900

    ivtests/array_slice_contact: add tb

--- ivtest/ivltests/array_slice_concat.v
+++ ivtest/ivltests/array_slice_concat.v
@@ -15,3 +15,21 @@ module ArraySliceWithNarrowStart(
   assign array_slice_6[2] = a_unflattened[{2'h0, start} > 3'h2 ? 3'h4 : {2'h0, start} + 3'h2];
   assign out = {array_slice_6[2], array_slice_6[1], array_slice_6[0]};
 endmodule
+
+module top;
+  reg [159:0] a;
+  reg start;
+  wire [95:0] out;
+  
+  ArraySliceWithNarrowStart dut(.a(a), .start(start), .out(out));
+   
+  initial begin
+     a = {32'h44444444, 32'h33333333, 32'h22222222, 32'h11111111};
+     start = 1;
+     if (out !== 96'h444444443333333322222222) begin
+	 $display("FAILED");
+	 $finish;
+     end
+     $display("PASSED");
+  end   
+endmodule
