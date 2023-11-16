483de9a75bd8bb3690e3a2f707f6bc7fc3ba9919
Sylvain Munaut <tnt@246tNt.com>
2023-11-16 09:25:38 +0100

    ivtest: Add test for partselect on enum inside pack struct
    See issue #1033
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ ivtest/ivltests/struct_enum_partsel.v
@@ -0,0 +1,24 @@
+module main;
+
+   typedef enum logic [2:0] {
+      ENUM_VAL = 3'b110
+   } enumtype;
+   
+   typedef struct packed {
+      enumtype e;
+   } structtype;
+
+   structtype s;
+
+   initial
+   begin
+      s.e = ENUM_VAL;
+      $display("s.e[2] = %d, s.e[1] = %d, s.e[0] = %d", s.e[2], s.e[1], s.e[0]);
+      if ((s.e[2] != 1'b1) || (s.e[1] != 1'b1) || (s.e[0] != 1'b0)) begin
+         $display("FAILED");
+         $finish;
+      end
+      $display("PASSED");
+   end
+
+endmodule
