8f006a22b48166d99250b42836375177ffbee875
Lars-Peter Clausen <lars@metafoo.de>
2023-12-27 09:14:26 -0800

    Add regression tests for invalid part select base expressions
    Check that a invalid part select base expression results in a compile
    error.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

---
+++ ivtest/ivltests/partsel_invalid_idx2.v
@@ -0,0 +1,14 @@
+// Check that a non-existent index into a vector results in an elaboration error.
+
+module test;
+
+  reg [31:0] a;
+  wire [1:0] b;
+
+  assign b = a[does_not_exist+:2]; // Error: Invalid base index
+
+  initial begin
+    $display("FAILED");
+  end
+
+endmodule
