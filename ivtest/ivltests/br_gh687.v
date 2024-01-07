fa2dfe690c5a2755a89c26f17f402376b63a7233
Martin Whitaker <icarus@martin-whitaker.me.uk>
2024-01-07 13:23:36 +0000

    Add regression test for synthesisable for loop check (issue #687)

---
+++ ivtest/ivltests/br_gh687.v
@@ -0,0 +1,25 @@
+module top;
+    logic [9:0] pipe = 0;
+    logic [4:0] i;
+    logic clk = 0;
+
+    always #1 clk = ~clk;
+
+    always_ff @(posedge clk) begin
+        for (i=0; i<9; i++) begin
+            pipe[i+1] <= pipe[i];
+        end
+        pipe[0] <= pipe[9];
+    end
+
+    initial begin
+        pipe[0] = 1'b1;
+        
+        for (int j=0; j<10; j++) begin
+            $display(pipe[9]);
+            #2;
+        end
+        
+        $finish(0);
+    end
+endmodule
