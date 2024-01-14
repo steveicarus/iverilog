fc9fcb0bf8006764d9f9529580e7430415172a0d
gatk555 <no address>
2024-01-14 22:14:06 +0000

    Regression test for vvp scheduler fix.
    This is for the latent bug exposed when testing the original fix for
    issue #1041. Contributed by gatk555 in PR #1065.

---
+++ ivtest/vpi/br_gh1041b.v
@@ -0,0 +1,18 @@
+module test(w);
+   input wire w;
+   wire a, b;
+
+   
+   initial begin
+      #11 $finish;
+   end
+
+   assign b = 0;
+   
+   assign a = !w | b;
+   
+   always @(a) begin
+      $display($time, ": Wire a is now ", a);
+   end
+endmodule
+   
