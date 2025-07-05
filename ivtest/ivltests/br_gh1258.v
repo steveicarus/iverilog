fd7029a299b838beccec03d76e2fba4ac44446af
Martin Whitaker <icarus@martin-whitaker.me.uk>
2025-07-05 22:52:52 +0100

    Add regression tests for issue #1258.

---
+++ ivtest/ivltests/br_gh1258.v
@@ -0,0 +1,12 @@
+module DFF (output reg Q, input CLK, D);
+        parameter [0:0] INIT = 1'b0;
+        initial Q = INIT;
+
+        specify
+                (posedge CLK => (Q : D)) = (480, 660);
+                $setup(D, posedge CLK, 576);
+        endspecify
+                
+        always @(posedge CLK)
+                Q <= D;
+endmodule
