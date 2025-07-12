db7af4e1714c23a4f47e1d719ffd9e1b9f3ca7df
Wilson Snyder <wsnyder@wsnyder.org>
2025-07-12 08:34:09 -0400

    Fix regression tests to not $error-fail assertions

--- ivtest/ivltests/sv_immediate_assert.v
+++ ivtest/ivltests/sv_immediate_assert.v
@@ -4,7 +4,7 @@ integer i = 1;
 
 initial begin
   assert(i == 1);
-  assert(i == 0);
+  // Fail: assert(i == 0);
   assert(i == 1) else $display("Check 3 : this shouldn't be displayed");
   assert(i == 0) else $display("Check 4 : this should be displayed");
   assert(i == 1) $display("Check 5 : this should be displayed");
@@ -15,8 +15,8 @@ initial begin
     else $display("Check 8 : this should be displayed");
 
   a_i_is_non_0 : assert(i == 0)
-    $display("Check 9 : this shouldn't be displayed");
-    else $error("Check 9 : this should be displayed");
+    $error("Check 9 : this shouldn't be displayed");
+    else $display("Check 9 : this should be displayed");
 
   a_i_is_1 : assert(i == 1)
     $display("Check 10 : this should be displayed");
