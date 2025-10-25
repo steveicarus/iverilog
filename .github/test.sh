70094ce5647f3adfaa3802b76de8510492df7ed6
Martin Whitaker <icarus@martin-whitaker.me.uk>
2025-10-25 15:49:11 +0100

    Disable PLI1 support in MSYS2/CLANG CI.

--- .github/test.sh
+++ .github/test.sh
@@ -9,7 +9,11 @@ status=0
 
 perl vvp_reg.pl || status=1
 
-perl vpi_reg.pl --with-pli1 || status=1
+if [ "x$1" = "xno-pli1" ] ; then
+    perl vpi_reg.pl || status=1
+else
+    perl vpi_reg.pl --with-pli1 || status=1
+fi
 
 python3 vvp_reg.py || status=1
 
