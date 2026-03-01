f61448859170b53e7cee72f6b18d619c747818ed
Cary R <cygcary@yahoo.com>
2026-03-01 15:33:15 -0800

    Update post branch directions for master

--- scripts/CREATE_BRANCH.sh
+++ scripts/CREATE_BRANCH.sh
@@ -14,6 +14,9 @@
 #
 #     git push -u origin v13-branch
 #
+# After this update the master branch to the next version and update
+# vvp/examples/hello.vvp to match the new version.
+#
 
 if [ $# -ne 1 ] ; then
     echo "Usage: CREATE_BRANCH.sh <major-number>"
