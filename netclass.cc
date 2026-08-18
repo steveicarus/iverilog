4d06b81f10ca76ef6b129a50173459d716f21695
Lars-Peter Clausen <lars@metafoo.de>
2026-08-17 21:57:21 -0700

    Support forward declared base classes
    SystemVerilog allows a forward declared class to be used as the base of a
    class that is declared before the base class definition.
    
        typedef class B;
        class C extends B;
        endclass
        class B;
        endclass
    
    The base type is currently elaborated while creating the derived class. At
    that point the base class has not been added to the scope yet and the base
    type does not resolve to a class.
    
    Create all classes first and bind their base classes in a second pass. Reject
    inheritance cycles before attaching the base class.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- netclass.cc
+++ netclass.cc
@@ -23,8 +23,8 @@
 
 using namespace std;
 
-netclass_t::netclass_t(perm_string name, const netclass_t*super)
-: name_(name), super_(super), class_scope_(0), definition_scope_(0), virtual_class_(false)
+netclass_t::netclass_t(perm_string name)
+: name_(name), super_(nullptr), class_scope_(0), definition_scope_(0), virtual_class_(false)
 {
 }
 
