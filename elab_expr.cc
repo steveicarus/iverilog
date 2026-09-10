c0d20878a1db6764e6b79ec0251a00d19c32740b
Michael Singer <michael@a-singer.de>
2026-09-10 06:39:35 +0200

    Implement toupper() and tolower() string functions

--- elab_expr.cc
+++ elab_expr.cc
@@ -4129,6 +4129,20 @@ NetExpr* PECallFunction::elaborate_expr_method_(Design*des, NetScope*scope,
 		  return sys_expr;
 	    }
 
+	    if (method_name == "toupper") {
+		  NetESFunc*sys_expr = new NetESFunc("$ivl_string_method$toupper",
+						     &netstring_t::type_string, 1);
+		  sys_expr->parm(0, sub_expr);
+		  return sys_expr;
+	    }
+
+	    if (method_name == "tolower") {
+		  NetESFunc*sys_expr = new NetESFunc("$ivl_string_method$tolower",
+						     &netstring_t::type_string, 1);
+		  sys_expr->parm(0, sub_expr);
+		  return sys_expr;
+	    }
+
 	    if (method_name == "substr") {
 		  if (parms_.size() != 2) {
 			cerr << get_fileline() << ": error: Method `substr()`"
